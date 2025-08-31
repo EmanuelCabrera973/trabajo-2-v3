1edb52fbce5e8f0eee5f09be639c20ac52f7f0d4
EmanuelCabrera973 <emanuel.cabrera@itscordoba.edu.ar>
2025-08-31 23:08:43 +0200

    script de importar oficinas

---
+++ crud/oficina/scripts/importar_oficinas.py
@@ -0,0 +1,44 @@
+import csv 
+import sys
+from django.db import transaction
+from django.core.exceptions import ValidationError
+from oficina.models import Oficina
+
+def run(*args):
+    if not args:
+        print("Error: proporcionar ruta del archivo")
+        print("uso:./manage.py runscript import_oficinas --script-args <ruta del archivo>")
+        
+    csv_file = args[0]
+    
+    try:
+        with open(csv_file,'r', encoding = 'utf8') as f:
+            reader = csv.DictReader(f)
+            oficinas_crear =[]
+            
+            for row in reader:
+                nombre = row.get('nombre')
+                nombre_corto=row.get('nombre_corto')
+                
+                if not nombre or not nombre_corto:
+                    print(f"error en la fila{row}. falta un campo")
+                    continue
+                
+                try:
+                    oficina = Oficina(nombre=nombre, nombre_corto=nombre_corto)
+                    oficina.full_clean()
+                    oficinas_crear.append(oficina)
+                except ValidationError as e:
+                    print(f"Error de validación en la fila {row}. Detalle: {e}")
+                except Exception as e:
+                    print(f"Error inesperado en la fila {row}. Detalle: {e}")
+                
+            with transaction.atomic():
+                Oficina.objects.bulk_create(oficinas_a_crear)
+                print(f"importacion de {len(oficinas_a_crear)} oficinas completada exitosamente")
+    
+    except FileNotFoundError:
+        print(f"Error no se encontro el el archivo {csv_file}")
+    except:
+        print(f"Ocurrio un erro inesperado en la importacion")
+    
